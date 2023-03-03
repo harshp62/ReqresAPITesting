846f0abd044fe2134c57107a7391d091c6934b99
harshp62 <102162110+harshp62@users.noreply.github.com>
2023-03-03 11:07:58 -0500

    extent

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,8 +5,9 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = "src\\test\\java\\features",
-glue = "steps", dryRun = false,monochrome = true, tags = "@Update",
-        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json"}
+glue = "steps", dryRun = false,monochrome = true,
+        plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json",
+        "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
